48b3e6d298fe3cd302fafbde275864f0a587ec7b
#𝐌𝐑𝐗 <138970072+MRX-JIBON-SARKAR@users.noreply.github.com>
2024-08-24 13:49:26 +0600

    Add files via upload

--- Auto-like.py
+++ Auto-like.py
@@ -1,16 +1,16 @@
 #Write By Zahirul
-#Github: https://github.com/MRX-JIBON-SARKAR
+#Github: https://github.com/Ghost3987
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-Set up the webdriver (replace the path with the location of your chromedriver)
+# Set up the webdriver (replace the path with the location of your chromedriver)
 driver = webdriver.Chrome('/path/to/chromedriver')
 
- List of Facebook IDs to like posts for
+# List of Facebook IDs to like posts for
 fb_ids = ['id1', 'id2', 'id3']
 
- Login to Facebook
+# Login to Facebook
 driver.get('https://www.facebook.com')
 email_field = driver.find_element_by_id('email')
 password_field = driver.find_element_by_id('pass')
@@ -18,7 +18,7 @@ email_field.send_keys('your_facebook_email')
 password_field.send_keys('your_facebook_password')
 password_field.send_keys(Keys.RETURN)
 
- Like posts for each Facebook ID
+# Like posts for each Facebook ID
 for fb_id in fb_ids:
     driver.get(f'https://www.facebook.com/{fb_id}')
     time.sleep(5)  # Wait for the page to load
