7f6649a04c395572625fbfc02c90a95c0d164948
#𝐌𝐑𝐗 <138970072+MRX-JIBON-SARKAR@users.noreply.github.com>
2024-08-24 13:48:48 +0600

    Add files via upload

--- Auto-like.py
+++ Auto-like.py
@@ -1,5 +1,5 @@
 #Write By Zahirul
-Github: https://github.com/MRX-JIBON-SARKAR
+#Github: https://github.com/MRX-JIBON-SARKAR
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
